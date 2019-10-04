4f69e796be6f155cad7defcce067ecfeef933c88
Ania <ania.makes@gmail.com>
2019-10-04 16:42:07 +0100

    robot is placed correctly

--- index.js
+++ index.js
@@ -4,16 +4,38 @@ let toy = {
     f: null
 }
 
+const clear = () => {
+    return toy = {
+        x: null,
+        y: null,
+        f: null
+    }
+}
+
+const validateDirection = (direction) => {
+    return direction === "NORTH" || direction === "EAST" || direction === "SOUTH" || direction === "WEST"
+}
+
 const validateNumber = (number) => {
     return Number.isInteger(number) && number >= 0 && number <= 4
 }
 
 const place = (x, y, f) => {
-    
+    if(validateNumber(x) && validateNumber(y) && validateDirection(f)){
+        toy = {
+            x: x,
+            y: y,
+            f: f
+        }
+    }
+
+    return toy;
 }
 
 
 module.exports = {
     validateNumber,
+    validateDirection,
+    clear,
     place
 }
--- tests/commands.test.js
+++ tests/commands.test.js
@@ -1,4 +1,4 @@
-const { validateNumber, place } = require('../index')
+const { validateNumber, validateDirection, clear, place } = require('../index')
 
 describe('Number validator works', () =>{
     it('passes a number that is positive and smaller than 5', () => {
@@ -16,10 +16,25 @@ describe('Number validator works', () =>{
 
 })
 
+describe('Direction validation works', () => {
+    it('passes good directions', () => {
+        expect(validateDirection('NORTH')).toBeTruthy();
+        expect(validateDirection('EAST')).toBeTruthy();
+        expect(validateDirection('SOUTH')).toBeTruthy();
+        expect(validateDirection('WEST')).toBeTruthy();
+    });
+
+    it('fails bad directions', () =>{
+        expect(validateDirection('cat')).toBeFalsy();
+        expect(validateDirection('9')).toBeFalsy();
+    })
+
+})
+
 describe('Places the robot', () => {
 
     it('places the robot on board', () => {
-
+        clear();
         const placement = place(2, 1, 'NORTH');
         const expectedPlacemnt = {
             x : 2,
@@ -32,6 +47,7 @@ describe('Places the robot', () => {
     )
 
     it('does not place the robot if the coordinates are off-board', () => {
+        clear();
         const placement = place(6, 7, 'EAST');
         const expectedPlacemnt = {
             x : null,
@@ -55,6 +71,7 @@ describe('Places the robot', () => {
     })
 
     it('ignores bad input in y', () => {
+        clear();
         const placement = place(2, 'toy', 'EAST');
         const expectedPlacemnt = {
             x : null,
@@ -66,6 +83,7 @@ describe('Places the robot', () => {
     })
 
     it('ignores bad input in f', () => {
+        
         const placement = place(1, 2, 'SKY');
         const expectedPlacemnt = {
             x : null,
